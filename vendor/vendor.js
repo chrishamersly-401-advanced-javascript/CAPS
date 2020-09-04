918ecf1c91361d6c08ca0418224e1cc70780a24c
CHRIS HAMERSLY <CHRISTOPHERHAMERSLY@GMAIL.COM>
2020-09-03 20:45:54 -0700

    working on the queue

--- vendor/vendor.js
+++ vendor/vendor.js
@@ -1,90 +1,30 @@
 'use strict';
-
-const io= require('socket.io-client');
+const client = require('socket.io-client');
 const faker = require('faker');
 require('dotenv').config();
-const vendorSocket = io.connect('http://localhost:3001/caps');
-vendorSocket.emit('join', process.env.STORE_NAME);
-
 const randomOrderId = faker.random.number();
 const randomOrderName = faker.name.findName(); 
 const randomAddress = faker.address.streetAddress(); 
 const time = faker.date.recent();
 const storeName = process.env.STORE_NAME;
 
+const socket = client.connect('http://localhost:3001/caps');
 
-setInterval(()=>{
+socket.emit('join', storeName);
 
-    const order ={
-      event: 'pickup',
-      time: new Date(),
-        storename: storeName,
-        orderID: randomOrderId, 
-        customerName: randomOrderName,
-        address: randomAddress, 
-      }
+socket.on('delivered', (payload) => {
+  console.log(`Thank you for delivering ${payload.orderID}`);
+})
 
-console.log('driver picked up', order.orderID, time)
-  vendorSocket.emit('pickup', order);
-}, 5000);
+setInterval(() => {
 
-setInterval(()=>{
-
-  const order ={
-    event: 'pickup',
-    time: new Date(),
-      storename: storeName,
-      orderID: randomOrderId, 
-      customerName: randomOrderName,
-      address: randomAddress, 
-    }
-
-console.log('driver delivered', order.orderID, time)
-vendorSocket.emit('delivered', order);
-}, 5000);
+  let delivery = {
+    store : storeName, 
+    orderID : randomOrderId,
+    customer: randomOrderName,
+    address: randomAddress
+  }
+  socket.emit('pickup', delivery);
+}, 5000)
 
 
-
-
-
-
-
-
-
-
-
-// const faker = require('faker');
-// require('dotenv').config();
-// const emitter = require('../apps/lib/events');
-
-
-// const randomStore = faker.phone.phoneNumber();
-
-
-
-// const delivery = {
-//   store: randomStore,
-//   orderID: randomOrderId,
-//   customer: randomOrderName,
-//   address: randomAddress,
-// };
-
-// emitter.on('delivered', (payload) =>{
-//   console.log(`VENDOR: Thank you for delivering ${payload.orderID}`);
-// });
-
-
-// function start() {
-//   setInterval(() => {
-//     emitter.emit('pickup', delivery);
-//   }, 5000);
-// }
-
-
-// console.log('in the vendors');
-// start();
-
-// module.exports = { start };
-
-
- 
